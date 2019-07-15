75cb03cff669b31d38350d46f4ffea18d6e8b5e4
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2019-07-14 23:41:26 -0500

    Routing ready

--- src/App.js
+++ src/App.js
@@ -1,33 +1,60 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import FormBasic from "./Components/FormBasic";
 import Header from "./Components/Header";
+import Contact from "./Components/Contact";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Header />
-      <div className="container fancyBorder col-md-6">
-        <div className="row">
-          <div className="para">
-            <h1>Bla</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipiscing, elit taciti dis
-              suscipit sapien parturient ad, dapibus dictum netus sodales
-              dictumst. Arcu ac dictumst cum elementum tristique augue erat
-              tincidunt justo commodo, eleifend ullamcorper taciti mattis primis
-              ad mi posuere nec cursus habitasse, placerat mauris risus auctor
-              dapibus magna metus montes quam.
-            </p>
-            <FormBasic />
+    <Router>
+      <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-primary myNav">
+          <a class="navbar-brand" href="#">
+            The Navbar Component
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon" />
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li>
+                <Link to={"/"} className="nav-link">
+                  {" "}
+                  Home{" "}
+                </Link>
+              </li>
+              <li>
+                <Link to={"/Contact"} className="nav-link">
+                  Contact
+                </Link>
+              </li>
+              <li>
+                <Link to={"/Header"} className="nav-link">
+                  Form
+                </Link>
+              </li>
+            </ul>
           </div>
-        </div>
+        </nav>
 
+        <Switch>
+          <Route exact path="/" component={Header} />
+          <Route path="/Contact" component={Contact} />
+          <Route path="/Header" component={FormBasic} />
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/Components/FormBasic.js
+++ src/Components/FormBasic.js
@@ -34,6 +34,7 @@ class FormBasic extends React.Component {
 
   render() {
     return (
+      <div>
       <form>
         <input
           type="text"
@@ -52,6 +53,24 @@ class FormBasic extends React.Component {
           onChange={this.changeHandler}
         />
       </form>
+      <div className="container fancyBorder col-md-6">
+        <div className="row">
+          <div className="para">
+            <h1>Bla</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipiscing, elit taciti dis
+              suscipit sapien parturient ad, dapibus dictum netus sodales
+              dictumst. Arcu ac dictumst cum elementum tristique augue erat
+              tincidunt justo commodo, eleifend ullamcorper taciti mattis primis
+              ad mi posuere nec cursus habitasse, placerat mauris risus auctor
+              dapibus magna metus montes quam.
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
     );
   }
 }
